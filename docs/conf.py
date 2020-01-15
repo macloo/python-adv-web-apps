ba4927616f8d1d64230459acff398232d4526641
macloo <mmcadams@gmail.com>
2020-01-15 16:46:49 -0500

    installed sphinx-rtd-theme
    See https://sphinx-rtd-theme.readthedocs.io/en/latest/

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'python-adv-web-apps'
-copyright = '2020, Mindy McAdams'
+project = 'Python Beginners'
+copyright = '2020 Mindy McAdams'
 author = 'Mindy McAdams'
 
 
@@ -27,8 +27,13 @@ author = 'Mindy McAdams'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+import sphinx_rtd_theme
+
 extensions = [
+'sphinx_rtd_theme'
 ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +49,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
