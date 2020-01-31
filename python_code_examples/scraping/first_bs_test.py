cf3b2524833cb92fc288b7f1bb165cacdc0f8f77
macloo <mmcadams@gmail.com>
2020-01-31 12:11:37 -0500

    add first BeautifulSoup script

---
+++ python_code_examples/scraping/first_bs_test.py
@@ -0,0 +1,38 @@
+"""A first simple test-run of the BeautifulSoup module, using
+   a simple web page created just for this purpose.
+"""
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# open a web page file
+page = urlopen("https://weimergeeks.com/examples/scraping/example1.html")
+
+# parse that to create a BeautifulSoup object
+soup = BeautifulSoup(page, "html.parser")
+
+# print the first H1 element in the file
+print(soup.h1)
+
+# print ONLY THE TEXT in the first H1 element in the file
+print(soup.h1.text)
+
+# get all the TD elements that have the class 'city'
+city_list = soup.find_all( "td", class_="city" )
+
+# print only the text from each of those TD elements
+for city in city_list:
+    print( city.get_text() )
+
+# using find() gets only ONE element
+# here we assign that element to a new variable, phone_number
+phone_number = soup.find( id="call" )
+
+# print only the text from the element with the id 'call'
+print( phone_number.get_text() )
+
+# get the values of all the src attributes for all the IMG elements
+# on the web page, and print them, one per line
+image_list = soup.find_all('img')
+for image in image_list:
+    print( image.attrs['src'] )
