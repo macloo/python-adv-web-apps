8bb8bcded9a1465dd7919044dec7a75947e2cbd5
macloo <mmcadams@gmail.com>
2020-02-07 10:46:53 -0500

    small edit

--- python_code_examples/scraping/selenium_test1.py
+++ python_code_examples/scraping/selenium_test1.py
@@ -1,4 +1,5 @@
 # from https://sites.google.com/a/chromium.org/chromedriver/getting-started
+# 
 # This requires Selenium to be installed in your virtualenv
 # And you need to download and install chromedriver as explained here:
 # http://bit.ly/selenium-intro
@@ -10,7 +11,7 @@ from selenium import webdriver
 # NOTE this is my own path, not yours --
 driver = webdriver.Chrome('/Users/mcadams/Documents/python/chromedriver')
 
-driver.get('http://www.google.com/xhtml')
+driver.get('http://www.google.com/')
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
