9988239092a2799183db14ea6192996268978373
macloo <mmcadams@gmail.com>
2020-02-15 12:37:14 -0500

    edit files

--- python_code_examples/dictionaries/dictreader_example.py
+++ python_code_examples/dictionaries/dictreader_example.py
@@ -1,33 +1,32 @@
-"""
-convert a csv file to dictionaries - each row is one dict
-Python 3.6 and not before
+"""convert a csv file to dictionaries - each row is one dict
+    in a single Python list
+    Python 3.6 and later
 """
 import csv
 
 # open a CSV file
-# note - must have column headings in top row
+# note - the CSV must have column headings in top row
 datafile = open("sample.csv", newline='')
 
-# create list of OrderedDicts as of Python 3.6
+# create list of OrderedDicts - as of Python 3.6
 my_reader = csv.DictReader(datafile)
 
+# crerate new empty list
+my_list = []
 
-# commented out because both loops can't run
-#    - this works -
-# my_list = []
-# for row in my_reader:
-#     my_list.append( dict(row) )
-#
-# print(my_list[1]["Title"])
-
-
-# write it all out to a new file
-targetfile = open("newdict.py", 'w')
+# loop over CSV row by row and write each row as a dictionary into the list
 for row in my_reader:
-    # we convert each row to a string and add a newline
-    targetfile.write( str(dict(row)) + "\n" )
-targetfile.close()
+    my_list.append( dict(row) )
 
+# print some examples from the new list
+print("Printing second item:")
+print(my_list[1])
+print("What is the data tyoe of the item?")
+print(type(my_list[1]))
+print("Printing value of key \"Title\" for second item:")
+print(my_list[1]["Title"])
+print("Printing value of key \"Author\" for second item:")
+print(my_list[1]["Author"])
 
 # close original csv file
 datafile.close()
