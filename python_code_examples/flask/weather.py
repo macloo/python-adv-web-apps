42d9d9101268fa9885563a77efcef9e8e72bfa78
macloo <mmcadams@gmail.com>
2020-03-01 17:45:03 -0500

    added try except to scripts

---
+++ python_code_examples/flask/weather.py
@@ -0,0 +1,39 @@
+"""a demo Flask app to access an API
+   the idea for this example came from:
+   https://medium.com/free-code-camp/how-to-build-a-web-app-using-pythons-flask-and-google-app-engine-52b1bb82b221
+"""
+
+import requests
+from flask import Flask
+app = Flask(__name__)
+
+# this is not a real key
+API_KEY = '12345abcXYZ'
+
+# get weather by U.S. zip code
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?zip={},us&mode=json&units=imperial&appid={}')
+
+def query_api(zip):
+    """submit the API query using variables for zip and API_KEY"""
+    try:
+        # print(API_URL.format(zip, API_KEY))
+        data = requests.get(API_URL.format(zip, API_KEY)).json()
+    except Exception as exc:
+        print(exc)
+        data = None
+    return data
+
+@app.route('/weather/<zip>')
+def result(zip):
+    # get the json file from the OpenWeather API
+    resp = query_api(zip)
+    # construct a string using the json data items for temp and
+    # description
+    try:
+        text = resp["name"] + " temperature is " + str(resp["main"]["temp"]) + " degrees Fahrenheit with " + resp["weather"][0]["description"] + "."
+    except:
+        text = "There was an error.<br>Did you include a valid zip code in the URL?"
+    return text
+
+if __name__ == '__main__':
+    app.run(debug=True)
