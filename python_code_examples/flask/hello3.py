9a96861a3a6afcc2af465d51fc51b2a5a44a3961
macloo <mmcadams@gmail.com>
2020-03-01 13:20:44 -0500

    add file

---
+++ python_code_examples/flask/hello3.py
@@ -0,0 +1,21 @@
+"""basic Flask app - demo of using a variable in a route"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    greet = '<h1>Hello, Gators!</h1>'
+    link = '<p><a href="user/Albert">Click me!</a></p>'
+    return greet + link
+
+@app.route('/user/<name>')
+def user(name):
+    personal = f'<h1>Hello, {name}!</h1>'
+    # above - the curly braces {} hold a variable; when this runs,
+    # the value will replace the braces and the variable name
+    instruc = '<p>Change the name in the <em>browser address bar</em> \
+        and reload the page.</p>'
+    return personal + instruc
+
+if __name__ == '__main__':
+    app.run(debug=True)
