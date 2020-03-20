f95935c93ee0cd1bb0d275205cdc4c83b1fe3a40
macloo <mmcadams@gmail.com>
2020-03-20 18:30:58 -0400

    add frozen section

---
+++ python_code_examples/flask/freeze.py
@@ -0,0 +1,14 @@
+"""standard freeze script"""
+
+from flask_frozen import Freezer
+
+# instead of "filename,"" below, use the name of the file that
+# runs YOUR Flask app
+from filename import app
+
+app.config['FREEZER_RELATIVE_URLS'] = True
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
