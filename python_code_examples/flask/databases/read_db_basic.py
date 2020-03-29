b8886abe63a16b5efb5b748d9b474b2d8242cf0d
macloo <mmcadams@gmail.com>
2020-03-29 16:09:10 -0400

    add new script

---
+++ python_code_examples/flask/databases/read_db_basic.py
@@ -0,0 +1,49 @@
+""" read from a SQLite database and return data """
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# the name of the database; add path if necessary
+db_name = 'sockmarket.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+# each table in the database needs a class to be created for it
+# db.Model is required - don't change it
+# identify all columns by name and data type
+class Sock(db.Model):
+    __tablename__ = 'socks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    style = db.Column(db.String)
+    color = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    updated = db.Column(db.String)
+
+#routes
+
+@app.route('/')
+def index():
+    try:
+        socks = Sock.query.filter_by(style='mini').order_by(Sock.name).all()
+        sock_text = '<ul>'
+        for sock in socks:
+            sock_text += '<li>' + sock.name + ', ' + sock.color + '</li>'
+        sock_text += '</ul>'
+        return sock_text
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
