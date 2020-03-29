68abadacb9bb4d3153c0b6a4a6530655f9e5cef8
macloo <mmcadams@gmail.com>
2020-03-29 09:44:26 -0400

    add db files

---
+++ python_code_examples/flask/databases/test_local_sqlite_db.py
@@ -0,0 +1,37 @@
+"""
+    test a SQLite database connection locally
+    assumes database file is in same location
+    as this .py file
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+app = Flask(__name__)
+
+# change to name of your database; add path if necessary
+db_name = 'sockmarket.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+# NOTHING BELOW THIS LINE NEEDS TO CHANGE
+# this route will test the database connection and nothing more
+@app.route('/')
+def testdb():
+    try:
+        db.session.query('1').from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
