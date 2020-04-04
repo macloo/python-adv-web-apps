25fd993edbec17abfa762bf24c63d93a4f4e1543
macloo <mmcadams@gmail.com>
2020-04-04 10:58:15 -0400

    update db write app

--- python_code_examples/flask/databases/flask_db_write/write_db.py
+++ python_code_examples/flask/databases/flask_db_write/write_db.py
@@ -1,4 +1,6 @@
-""" write to a SQLite database with forms, templates """
+""" write to a SQLite database with forms, templates
+    add new record, delete a record, edit/update a record
+    """
 
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
@@ -48,12 +50,13 @@ class Sock(db.Model):
         self.price = price
         self.updated = updated
 
+# +++++++++++++++++++++++
 # forms with Flask-WTF
 
-# form for add_record and update_record
+# form for add_record and edit_or_delete
 # each field includes validation requirements and messages
 class AddRecord(FlaskForm):
-    # id used only by update_record
+    # id used only by update/edit
     id_field = HiddenField()
     name = StringField('Sock name', [ InputRequired(),
         Regexp(r'^[A-Za-z\s\-\']+$', message="Invalid sock name"),
@@ -77,6 +80,12 @@ class AddRecord(FlaskForm):
     # updated - date - handled in the route
     updated = HiddenField()
     submit = SubmitField('Add/Update Record')
+
+# small form
+class DeleteForm(FlaskForm):
+    id_field = HiddenField()
+    purpose = HiddenField()
+    submit = SubmitField('Delete This Sock')
 
 # +++++++++++++++++++++++
 # get local date - does not account for time zone
@@ -133,17 +142,93 @@ def add_record():
                 ), 'error')
         return render_template('add_record.html', form1=form1)
 
+# select a record to edit or delete
+@app.route('/select_record/<letters>')
+def select_record(letters):
+    # alphabetical lists by sock name, chunked by letters between _ and _
+    # .between() evaluates first letter of a string
+    a, b = list(letters)
+    socks = Sock.query.filter(Sock.name.between(a, b)).order_by(Sock.name).all()
+    return render_template('select_record.html', socks=socks)
 
-# error handlers
+# edit or delete - come here from form in /select_record
+@app.route('/edit_or_delete', methods=['POST'])
+def edit_or_delete():
+    id = request.form['id']
+    choice = request.form['choice']
+    sock = Sock.query.filter(Sock.id == id).first()
+    # two forms in this template
+    form1 = AddRecord()
+    form2 = DeleteForm()
+    return render_template('edit_or_delete.html', sock=sock, form1=form1, form2=form2, choice=choice)
+
+# result of delete - this function deletes the record
+@app.route('/delete_result', methods=['POST'])
+def delete_result():
+    id = request.form['id_field']
+    purpose = request.form['purpose']
+    sock = Sock.query.filter(Sock.id == id).first()
+    if purpose == 'delete':
+        db.session.delete(sock)
+        db.session.commit()
+        message = f"The sock {sock.name} has been deleted from the database."
+        return render_template('result.html', message=message)
+    else:
+        # this calls an error handler
+        abort(405)
+
+# result of edit - this function updates the record
+@app.route('/edit_result', methods=['POST'])
+def edit_result():
+    id = request.form['id_field']
+    # call up the record from the database
+    sock = Sock.query.filter(Sock.id == id).first()
+    # update all values
+    sock.name = request.form['name']
+    sock.style = request.form['style']
+    sock.color = request.form['color']
+    sock.quantity = request.form['quantity']
+    sock.price = request.form['price']
+    # get today's date from function, above all the routes
+    sock.updated = stringdate()
+
+    form1 = AddRecord()
+    if form1.validate_on_submit():
+        # update database record
+        db.session.commit()
+        # create a message to send to the template
+        message = f"The data for sock {sock.name} has been updated."
+        return render_template('result.html', message=message)
+    else:
+        # show validaton errors
+        sock.id = id
+        # see https://pythonprogramming.net/flash-flask-tutorial/
+        for field, errors in form1.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form1, field).label.text,
+                    error
+                ), 'error')
+        return render_template('edit_or_delete.html', form1=form1, sock=sock, choice='edit')
+
+
+# +++++++++++++++++++++++
+# error routes
+# https://flask.palletsprojects.com/en/1.1.x/patterns/apierrors/#registering-an-error-handler
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', error=e), 404
+    return render_template('error.html', pagetitle="404 Error - Page Not Found", pageheading="Page not found (Error 404)", error=e), 404
+
+@app.errorhandler(405)
+def form_not_posted(e):
+    return render_template('error.html', pagetitle="405 Error - Form Not Submitted", pageheading="The form was not submitted (Error 405)", error=e), 405
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html', error=e), 500
+    return render_template('error.html', pagetitle="500 Error - Internal Server Error", pageheading="Internal server error (500)", error=e), 500
 
+# +++++++++++++++++++++++
 
 if __name__ == '__main__':
     app.run(debug=True)
