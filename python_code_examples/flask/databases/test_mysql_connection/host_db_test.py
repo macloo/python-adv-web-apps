6b01388fec5884166599e1714b84026fc067d2a3
Mindy McAdams <mmcadams@gmail.com>
2023-04-03 18:18:47 -0400

    updates for SQLAlchemy 2.0

--- python_code_examples/flask/databases/test_mysql_connection/host_db_test.py
+++ python_code_examples/flask/databases/test_mysql_connection/host_db_test.py
@@ -1,6 +1,7 @@
 """
-test for a MySQL database connection on your hosted website
-both the database and this script must be on the server
+test for a -MySQL- database connection on your hosted website
+both the database and this script must be on the same server
+requires PyMySQL, Flask-SQLAlchemy, Flask
 """
 
 import pymysql
@@ -8,42 +9,45 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
-# the next line is necessary with cPanel deployment
-application = app
 
-# make sure the username, password and database name are correct
+# make sure the database username, database password and
+# database name are correct
 username = 'something'
 password = 'some_pw'
 userpass = 'mysql+pymysql://' + username + ':' + password + '@'
 # keep this as is for a hosted website
 server  = '127.0.0.1'
-# change to YOUR database name, with a slash added as shown
+# CHANGE to YOUR database name, with a slash added as shown
 dbname   = '/database_name'
-# no socket
+# there is no socket
 
 
-# change NOTHING below
-
+# CHANGE NOTHING BELOW
 # put them all together as a string that shows SQLAlchemy where the database is
 app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# this variable, db, will be used for all SQLAlchemy commands
-db = SQLAlchemy(app)
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
 
 # NOTHING BELOW THIS LINE NEEDS TO CHANGE
-# this route will test the database connection and nothing more
+# this route will test the database connection - and nothing more
 @app.route('/')
 def testdb():
     try:
-        db.session.query('1').from_statement(text('SELECT 1')).all()
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
         return '<h1>It works.</h1>'
     except Exception as e:
-        # see Terminal for description of the error
-        print("\nThe error:\n" + str(e) + "\n")
-        return '<h1>Something is broken.</h1>'
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 if __name__ == '__main__':
     app.run(debug=True)
--- python_code_examples/flask/databases/test_mysql_connection/local_db_test.py
+++ python_code_examples/flask/databases/test_mysql_connection/local_db_test.py
@@ -1,5 +1,7 @@
 """
-test for a local -MySQL- database connection
+test for a local -MySQL- database connection with XAMPP
+requires PyMySQL, Flask-SQLAlchemy, Flask
+.
 make sure your virtualenv is activated!
 make sure you have "started all" in XAMPP!
 code below works for a MySQL database in XAMPP
@@ -11,6 +13,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
 
 # assumes you did not create a password for your database
@@ -19,33 +24,36 @@ app = Flask(__name__)
 username = 'root'
 password = ''
 userpass = 'mysql+pymysql://' + username + ':' + password + '@'
-server  = '127.0.0.1'
-# change to YOUR database name, with a slash added as shown
+server   = '127.0.0.1'
+# CHANGE to YOUR database name, with a slash added as shown
 dbname   = '/database_name'
 
 # this socket is going to be very different on a WINDOWS computer
 # try 'C:/xampp/mysql/mysql.sock'
 socket   = '?unix_socket=/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
 
+# CHANGE NOTHING BELOW
 # put them all together as a string that shows SQLAlchemy where the database is
 app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname + socket
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# this variable, db, will be used for all SQLAlchemy commands
-db = SQLAlchemy(app)
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
 
 # NOTHING BELOW THIS LINE NEEDS TO CHANGE
-# this route will test the database connection and nothing more
+# this route will test the database connection - and nothing more
 @app.route('/')
 def testdb():
     try:
-        db.session.query('1').from_statement(text('SELECT 1')).all()
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
         return '<h1>It works.</h1>'
     except Exception as e:
-        # see Terminal for description of the error
-        print("\nThe error:\n" + str(e) + "\n")
-        return '<h1>Something is broken.</h1>'
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 if __name__ == '__main__':
     app.run(debug=True)
