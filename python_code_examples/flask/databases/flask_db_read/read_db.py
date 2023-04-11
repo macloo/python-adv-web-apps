602b51fd2cdb646c3265b6e3be702b5ca68d96c1
Mindy McAdams <mmcadams@gmail.com>
2023-04-11 11:01:40 -0400

    updates for SQLAlchemy 2.0

--- python_code_examples/flask/databases/flask_db_read/read_db.py
+++ python_code_examples/flask/databases/flask_db_read/read_db.py
@@ -2,22 +2,28 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+import os.path
+from flask_bootstrap import Bootstrap5
 
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
-
-# Flask-Bootstrap requires this line
-Bootstrap(app)
-
-# the name of the database; add path if necessary
+# change string to the name of your database; add path if necessary
 db_name = 'sockmarket.db'
+# note - path is necessary for a SQLite db!!!
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, db_name)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# this variable, db, will be used for all SQLAlchemy commands
-db = SQLAlchemy(app)
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap5(app)
 
 # each table in the database needs a class to be created for it
 # db.Model is required - don't change it
@@ -37,14 +43,17 @@ class Sock(db.Model):
 @app.route('/')
 def index():
     # get a list of unique values in the style column
-    styles = Sock.query.with_entities(Sock.style).distinct()
+    styles = db.session.execute(db.select(Sock)
+        .with_only_columns(Sock.style).distinct())
     return render_template('index.html', styles=styles)
 
 
 @app.route('/inventory/<style>')
 def inventory(style):
     try:
-        socks = Sock.query.filter_by(style=style).order_by(Sock.name).all()
+        socks = db.session.execute(db.select(Sock)
+            .filter_by(style=style)
+            .order_by(Sock.name)).scalars()
         return render_template('list.html', socks=socks, style=style)
     except Exception as e:
         # e holds description of the error
@@ -54,4 +63,5 @@ def inventory(style):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=4999, debug=True)
