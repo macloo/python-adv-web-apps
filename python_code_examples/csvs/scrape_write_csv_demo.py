d7158daabd17a57a1015fed3d6365abfd29f1e0e
Mindy McAdams <mmcadams@gmail.com>
2025-02-28 13:41:37 -0500

    modify basic CSV writing script

--- python_code_examples/csvs/scrape_write_csv_demo.py
+++ python_code_examples/csvs/scrape_write_csv_demo.py
@@ -1,4 +1,4 @@
-"""example script for scraping a table and writing the data to a CSV file"""
+"""example script for scraping a table and writing the data to a new CSV file"""
 
 import csv
 
@@ -7,13 +7,16 @@ from bs4 import BeautifulSoup
 page = requests.get("https://weimergeeks.com/examples/scraping/example1.html")
 soup = BeautifulSoup(page.text, 'html.parser')
 
-# open new file for writing -
+# open new empty file for writing -
 csvfile = open('table_scrape.csv', 'w', newline='', encoding='utf-8')
 
 # make a new variable - c - for Python's CSV writer object -
 c = csv.writer(csvfile)
 
-# there is only one table on this page - get it with find()
+# write a header row to the CSV file
+c.writerow( ['Capital City', 'Country'] )
+
+# there is only one table on this page - so get it with find()
 table = soup.find('table')
 
 # get all rows from table
@@ -23,14 +26,15 @@ rows = table.find_all("tr")
 for row in rows:
     # a temporary list that will be overwritten each time the loop runs
     tmp_row = []
-    # get all cells from row
+    # get all cells from one row
     cells = row.find_all('td')
     # loop over all the td's in the current row - a loop inside a loop
     for cell in cells:
         # add contents of one td (cell) to the list
         tmp_row.append( cell.text )
-    # when all cells in one row have been added to the list,
-    # write one new row to the csv
+    # after all cells in one row have been added to the list,
+    # write that one row to the csv - 
+    # then the loop will run again as long as there are rows left
     c.writerow(tmp_row)
 
 # close the file
